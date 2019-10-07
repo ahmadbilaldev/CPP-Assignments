967fce1728325172bcd67dbce5fab306646de655
Ahmad Bilal <ahmadbilaldev@gmail.com>
2019-10-07 23:40:46 +0500

    👌 IMPROVE: Main program

--- Data Structures/Linked List/main.cpp
+++ Data Structures/Linked List/main.cpp
@@ -7,6 +7,7 @@ int main()
 {
 	list linkList;
 
+	// Insert Nodes.
 	linkList.insert(1);
 	linkList.insert(2);
 	linkList.insert(3);
@@ -17,25 +18,21 @@ int main()
 		 << "Check the reverse function:" << endl;
 	linkList.checkReverse();
 
-	// 	linkList.insertAtBegin(50);
-	// 	cout << endl
-	// 		 << "Now after adding 50 at the beginning my Link List is" << endl;
-	// 	linkList.print();
+	linkList.insertAtStart(50);
+	cout << endl
+		 << "Adding 50 at the beginning" << endl;
+	linkList.print();
 
-	// 	linkList.insertAtBegin(5);
-	// 	cout << "Now after adding 5 at the beginning my Link List is" << endl;
-	// 	linkList.print();
+	linkList.insertAtEnd(0);
+	cout << "Adding 0 at the end" << endl;
+	linkList.print();
 
-	// 	linkList.insertAtEnd(0);
-	// 	cout << "Now after adding 0 at the end my Link List is" << endl;
-	// 	linkList.print();
+	linkList.updateValueByIndex(9, 1);
 
-	// 	linkList.updateValueByIndex(201, 5);
-
-	// 	cout << "Now after Updating a node having value of 200 at the 5th index in the list by new value 201 my Link List is" << endl;
-	// 	linkList.print();
+	cout << "Updating the 5th node in the list by new value 9" << endl;
+	linkList.print();
 
 	// 	linkList.updateValue(99, 5);
 	// 	cout << "Now after Updating a node having previous value of 5 in the list by new value 150 my Link List is" << endl;
 	// 	linkList.print();
-	// }
+}
